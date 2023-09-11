c36e19ab19ab65afa3ea19db54c565172445eeba
AbhinavBawankule <abhinavbawankule@gmail.com>
2023-09-11 13:19:01 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from "./routes/home";
+import UserProfile from './routes/account';
 import './App.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>
+  },
+  {
+    path: "/Account-Settings",
+    element: <UserProfile/>
   },
 ]);
 
--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -1,5 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 export default function Home() {
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+    let path = "/Account-Settings"; 
+    navigate(path);
+  }
     return (
-        <div>Home</div>
+        <div>Home
+            <button color="primary" className="square"
+            onClick={routeChange}
+              > 
+              Login
+            </button>
+        </div>
+        
     );
 }
