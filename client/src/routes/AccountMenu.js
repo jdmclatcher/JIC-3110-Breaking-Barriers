aac70c48c9e41ea6602234d6cd6c2cb616549fa8
Bryan Kim <bkim2001@gmail.com>
2023-10-02 15:56:17 -0400

    create user signup page

--- client/src/routes/AccountMenu.js
+++ client/src/routes/AccountMenu.js
@@ -1,43 +1,17 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-const UserProfile = () => {
-  const [userData, setUserData] = useState({
-    username: 'JohnDoe123',
-    email: 'johndoe@example.com',
-    password: '******'
-  });
+import AccountForm from "../components/AccountForm";
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
+const UserProfile = () => {
   const navigate = useNavigate();
   const navigateToHome = () => {
     navigate('/');
-}
-
+  }
 
   return (
-    <div style={{ padding: '20px', width: '300px', border: '1px solid gray', borderRadius: '5px' }}>
+    <div>
       <button onClick={navigateToHome}>Back to Home</button>
-      <h2>User Profile</h2>
-
-      <div style={{ marginBottom: '10px' }}>
-        <label>Username: </label>
-        <input type="text" name="username" value={userData.username} onChange={handleChange} />
-      </div>
-
-      <div style={{ marginBottom: '10px' }}>
-        <label>Password: </label>
-        <input type="password" name="password" value={userData.password} onChange={handleChange} />
-      </div>
-
-      <div style={{ marginTop: '20px' }}>
-        <button>Save Changes</button>
-      </div>
+      <AccountForm/>   
     </div>
   );
 }
