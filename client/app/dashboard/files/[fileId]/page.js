5aac24f676eaa13069eee30bf2db4d26ba0787a6
Bryan Kim <bkim2001@gmail.com>
2023-10-16 21:50:10 -0400

    create view pdf page

--- client/app/dashboard/files/[fileId]/page.js
+++ client/app/dashboard/files/[fileId]/page.js
@@ -1,7 +1,28 @@
+'use client';
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import {Document, Page} from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
 const ViewFile = () => {
+    const {fileId, fileName} = useParams();
+    console.log(fileName)
+
+    const [numPages, setNumPages] = useState();
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
+
     return (
         <div className="file-container">
-            FILE
+            {fileId}
+            <Document file={``} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber}/>
+            </Document>
         </div>
     );
 }
