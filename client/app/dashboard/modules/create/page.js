b3d01b88b01dde30a5b4f39f5e9d52aff0aba44d
Iley, Tanner Michael <tiley2002@icloud.com>
2023-11-13 19:19:48 -0500

    update

---
+++ client/app/dashboard/modules/create/page.js
@@ -0,0 +1,59 @@
+'use client';
+import { useRef } from "react";
+import Link from "next/link";
+import { useRouter } from 'next/navigation';
+
+const CreateModulePage = () => {
+    const router = useRouter();
+    const titleRef = useRef();
+    const descriptionRef = useRef();
+
+
+    const handleSubmit = async () => {
+        const moduleData = {
+            administrator_id: "administrator1",
+            module_id: 1,
+            module_title: titleRef.current.value,
+            module_description: descriptionRef.current.value,
+        }
+        let response = await fetch(`http://localhost:${process.env.NEXT_PUBLIC_SERVER_PORT}/module/create`, {
+            method: 'POST',
+            body: JSON.stringify(moduleData),
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.status === 200) {
+            alert("module created successfully");
+            router.push('/dashboard');
+        } else if (response.status === 500) {
+            alert("Failed to create module");
+        }
+    }
+
+    return (
+        <div className="modules-container">
+            <Link href="/dashboard">Back to Dashboard</Link>
+            <div>
+                <Link href="/dashboard/modules">Back to modules</Link>
+            </div>
+            <h1 className="modules-header">Create module</h1>
+            <div className="modules-list">
+                <form onSubmit={handleSubmit} className="form">
+                    <label htmlFor="email">
+                        <p>module Title</p>
+                        <input ref={titleRef} required type="text" />
+                    </label>
+                    <label htmlFor="password">
+                        <p>module Description</p>
+                        <input ref={descriptionRef} required type="text" />
+                    </label>
+                    <button type="submit">Create Module</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default CreateModulePage;
