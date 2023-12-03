ef67c922fb3328a22df353f1a21ed965101a0c8d
Bryan Kim <bkim2001@gmail.com>
2023-12-03 14:47:46 -0500

    add basic styling

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,10 +1,14 @@
 "use client";
+import "./DashboardPage.css";
 
 const DashboardPage = () => {
   return (
-    <div>
-      Welcome to Breaking barriers. Please select a module to get started. TODO:
-      STYLE ME AND ADD CONTENT
+    <div class="main-page">
+      <div class="bg-gray-200 p-8 rounded rounder-lg">
+        <h1>
+          Welcome to Breaking barriers. Please use the sidebar to get started.
+        </h1>
+      </div>
     </div>
   );
 };
